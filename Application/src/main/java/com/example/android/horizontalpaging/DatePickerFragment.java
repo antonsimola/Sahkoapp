43a79d784f0918cfa9ae8de6a9a9ba09fbdada44
nuKle <anton.simola@gmail.com>
2015-04-17 16:39:21 +0300

    Kokeillaas, addasin muutaman tiedoston

---
+++ Application/src/main/java/com/example/android/horizontalpaging/DatePickerFragment.java
@@ -0,0 +1,40 @@
+package com.example.android.horizontalpaging;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by Anton on 17.4.2015.
+ */
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+    boolean aloitusaika;
+    public DatePickerFragment() {
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        TextView textAloitus =(TextView) getActivity().findViewById(R.id.aika_aloitus);
+        textAloitus.setText(day+"." + month +"."+ year);
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(),"timePicker");
+    }
+}
---
+++ Application/src/main/java/com/example/android/horizontalpaging/TimePickerFragment.java
@@ -0,0 +1,40 @@
+package com.example.android.horizontalpaging;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Anton on 17.4.2015.
+ * http://developer.android.com/guide/topics/ui/controls/pickers.html
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView textAloitus =(TextView) getActivity().findViewById(R.id.aika_aloitus);
+        if (minute < 10) {
+            textAloitus.setText(textAloitus.getText() + " klo " + hourOfDay + ":0" + minute);
+        } else{
+            textAloitus.setText(textAloitus.getText() + " " + hourOfDay + ":" + minute);
+        }
+    }
+}
