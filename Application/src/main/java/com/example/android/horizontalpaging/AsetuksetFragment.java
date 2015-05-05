f1b5fd20f5f0fb516840d467778a61fcde9926db
nuKle <anton.simola@gmail.com>
2015-05-05 12:51:37 +0300

    Lisätty muutama puuttunut tiedosto committiin

---
+++ Application/src/main/java/com/example/android/horizontalpaging/AsetuksetFragment.java
@@ -0,0 +1,42 @@
+package com.example.android.horizontalpaging;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AsetuksetFragment extends Fragment {
+
+
+    public AsetuksetFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayList<String> array_lang = new ArrayList<>();
+        array_lang.add("suomi");
+        array_lang.add("ruotsi");
+        array_lang.add("englanti");
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.simple_textview, array_lang);
+        View rootView = inflater.inflate(R.layout.fragment_asetukset, container, false);
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_language);
+
+        spinner.setAdapter(adapter);
+
+        return rootView;
+    }
+
+}
---
+++ Application/src/main/java/com/example/android/horizontalpaging/LoginDialogFragment.java
@@ -0,0 +1,38 @@
+package com.example.android.horizontalpaging;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final MainActivity act = (MainActivity) getActivity();
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_login, null))
+                // Add action buttons
+                .setPositiveButton("Kirjaudu sisään", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        act.setLoggedIn(true);
+                    }
+                });
+        return builder.create();
+    }
+}
